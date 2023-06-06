bf897eb38b051bdfc2713749997f88308ba8add4
MrAndriy <59733657+MrAndriy@users.noreply.github.com>
2023-06-06 16:59:08 +0000

    feat: init

---
+++ script.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+import { log } from "console";
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: "Andriy",
+    },
+  });
+
+  log(user);
+}
+
+main()
+  .catch((err) => console.error(err))
+  .finally(async () => await prisma.$disconnect());
